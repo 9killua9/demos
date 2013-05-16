78c3f05c704fd886abf322ae0dc883b87a6c08d8
Leandro Salar <info@reiatsu.com.ar>
2013-05-16 18:26:32 -0300

    Nueva app ?

--- js/gralCorazon.js
+++ js/gralCorazon.js
@@ -4,8 +4,36 @@
 ============================ */
 $(document).ready(function(){
     $(".bot1Volv").on('click',function(){
-            window.localStorage.setItem("prov", "");
             location.href="index.html";
+    });
+
+    $(".saca img, .saca").click(function(){
+        n1 = $("input[name=nombre1]").val();
+        n2 = $("input[name=nombre2]").val();
+        if(n1 != "" && n2 != "")
+        {
+            $(".estilosde").fadeOut("fast");
+            com1 = n1.length;
+            com2 = n2.length;
+
+            if(com1>=com2)
+            {
+                cien = com1;
+                saca = com2;
+            }
+            else
+            {
+                cien = com2;
+                saca = com1;
+            }
+
+            porcent  = Math.round(Math.random()*90) * cien / 100;
+
+            $(".contenidoCorazonTexto").html('Coinciden en un '+parseInt(porcent)+'0%').show();
+            $(".contenidoCorazon img").addClass("loop");
+        }
+        else
+            alert("Complete los nombres para saber tu coincidencia.");
     });
 });
 
